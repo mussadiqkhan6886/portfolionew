54f1d2d9095066df4d7b00f3d52c8434ca013838
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-20 04:06:26 +0500

    completed sections

---
+++ sections/Idea.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Idea = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default Idea
---
+++ sections/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default Skills
