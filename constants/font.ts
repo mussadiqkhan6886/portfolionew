121f24160591b7cbfe0c2bec852bedb25cfe7437
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-20 04:24:00 +0500

    imported font

---
+++ constants/font.ts
@@ -0,0 +1,7 @@
+import {DM_Sans} from "next/font/google"
+
+export const dSans = DM_Sans({
+    weight: ["100", "200", "300", "400", "500", "600", "700", '800', "900"],
+    style: ["normal", "italic"],
+    subsets: ["latin"]
+}) 
