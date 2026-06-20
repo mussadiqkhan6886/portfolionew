a85ddbcea294cca67d1d22546b43c2d168ae2fdb
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-21 00:01:34 +0500

    right section done now header

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -5,12 +5,12 @@ import { HiArrowDownRight, HiOutlineGlobeAlt } from 'react-icons/hi2'
 
 const Hero = () => {
   return (
-    <section style={{backgroundImage: `URL(${"/img3.png"})`, backgroundSize: "contain", backgroundPosition: "top", backgroundRepeat: "no-repeat"}} className="w-full h-screen relative bg-gray/98">
+    <section style={{backgroundImage: `URL(${"/img3.png"})`, backgroundSize: "contain", backgroundPosition: "top", backgroundRepeat: "no-repeat"}} className="w-full h-screen relative bg-gray/98 overflow-hidden">
       <div className="absolute w-22 h-screen left-0 bg-gray" />
-      <div className="absolute flex justify-between w-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute flex justify-between w-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-37 items-center">
         <div className="bg-text text-white flex p-4 rounded-r-full items-center justify-between w-60">
           <div className="w-[50%] ml-7">
-            <h2 className='leading-[1.2] font-medium tracking-tighter'>Located <br /> in the <br /> Pakistan</h2>
+            <p className='leading-[1.2] font-medium tracking-tighter'>Located <br /> in the <br /> Pakistan</p>
           </div>
           <div className="w-[30%] flex justify-end">
             <div className='bg-gray aspect-square w-full flex items-center justify-center rounded-full '>
@@ -18,18 +18,16 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div>
-          <HiArrowDownRight />
-          <div>
-            <h2>Freelance <br /> Designer & Developer</h2>
-          </div>
+        <div className="flex flex-col mb-16 h-full justify-between pr-22">
+          <HiArrowDownRight size={24} color='white' />
+          <h2 className="text-white text-[32px] leading-tight tracking-tight">Freelance <br /> Designer & Developer</h2>
         </div>
       </div>
         <CurvedLoop
           marqueeText="Mussadiq Khan ✦"
           speed={2}
           curveAmount={0}
-          direction="right"
+          direction="left"
           interactive={false}
         />
     </section>
