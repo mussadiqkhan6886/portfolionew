bfe524c9abf3f46bed39a2b1a553ef73ddb0eba3
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-20 05:15:35 +0500

    hero image done

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <section>
-      
+    <section style={{backgroundImage: `URL(${"/img3.png"})`, backgroundSize: "contain", backgroundPosition: "top", backgroundRepeat: "no-repeat"}} className="w-full h-screen relative bg-gray/98">
+      <div className="absolute w-22 h-screen left-0 bg-gray" />
     </section>
   )
 }
