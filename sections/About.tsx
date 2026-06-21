6d933b5109c2e339a00314305c8fa92e7e4e482c
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-22 02:57:19 +0500

    animation done

--- sections/About.tsx
+++ sections/About.tsx
@@ -20,14 +20,8 @@ const About = () => {
 
       <div className="flex relative flex-row lg:flex-col gap-5 md:gap-18 h-70 sm:h-80 ">
         <article className="max-w-xs pr-3 md:pr-10">
-          <ScrollFloat
-          animationDuration={0.5}
-          ease='back.inOut(2)'
-          stagger={0.01}
-          >
             The combination of my passion for design, code & interaction
             positions me in a unique place in the web design world.
-          </ScrollFloat>
         </article>
 
         <FloatEffect output={[100, -200]} className="absolute right-0 md:right-20 bottom-10 md:bottom-30 lg:left-0 lg:bottom-0">
