92accb27729304ac7bbfab80702298b23b246710
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-22 02:54:29 +0500

    about aniamtion

--- sections/About.tsx
+++ sections/About.tsx
@@ -1,5 +1,6 @@
 import FloatEffect from '@/components/ui/FloatEffect'
 import MagnetText from '@/components/ui/MagnetEffect'
+import ScrollFloat from '@/components/ui/ScrollEffect'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,18 +8,26 @@ const About = () => {
   return (
     <section className="bg-white flex flex-col lg:flex-row justify-between gap-16 max-w-270 mx-auto px-5 md:px-0 pt-30 md:pt-40 lg:pb-20">
       <article className="max-w-2xl">
-        <p className="text-3xl leading-10">
-          Helping brands to stand out in the digital era. Together we will set
-          the new status quo. No nonsense, always on the cutting edge.
-        </p>
+        <ScrollFloat
+          animationDuration={0.5}
+          ease='back.inOut(2)'
+          stagger={0.01}
+          textClassName='text-3xl leading-10'
+        >
+          Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.
+        </ScrollFloat>
       </article>
 
       <div className="flex relative flex-row lg:flex-col gap-5 md:gap-18 h-70 sm:h-80 ">
         <article className="max-w-xs pr-3 md:pr-10">
-          <p>
+          <ScrollFloat
+          animationDuration={0.5}
+          ease='back.inOut(2)'
+          stagger={0.01}
+          >
             The combination of my passion for design, code & interaction
             positions me in a unique place in the web design world.
-          </p>
+          </ScrollFloat>
         </article>
 
         <FloatEffect output={[100, -200]} className="absolute right-0 md:right-20 bottom-10 md:bottom-30 lg:left-0 lg:bottom-0">
