afc10a9219d851fc51431dd4b213a505cc8c68c1
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-21 20:26:24 +0500

    hero

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -5,8 +5,8 @@ import { HiArrowDownRight } from 'react-icons/hi2'
 
 const Hero = () => {
   return (
-    <section style={{backgroundImage: `URL(${"/img3.png"})`, backgroundPosition: "top", backgroundRepeat: "no-repeat"}} className="w-full h-screen relative bg-gray/98 overflow-hidden bg-cover xl:bg-contain">
-      <div className="absolute w-22 h-screen left-0 bg-gray hidden lg:block" />
+    <section style={{backgroundImage: `URL(${"/img3.png"})`, backgroundPosition: "top", backgroundRepeat: "no-repeat"}} className="w-full h-dvh relative bg-gray/98 overflow-hidden bg-cover xl:bg-contain">
+      <div className="absolute w-22 h-dvh left-0 bg-gray hidden lg:block" />
       <div className="absolute w-screen h-screen inset-0 bg-black/10" />
       <div className="absolute flex justify-between w-full left-1/2 bottom-0 sm:top-1/2 -translate-x-1/2 sm:-translate-y-1/2 h-32 sm:h-37 items-center">
         <div className="bg-text text-white hidden sm:flex p-4 rounded-r-full items-center justify-between w-60">
