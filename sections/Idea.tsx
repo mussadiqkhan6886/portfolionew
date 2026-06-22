4e026276f23ace232dfdede27a5d96ad0c58f1c9
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-23 02:38:49 +0500

    fixed idea

--- sections/Idea.tsx
+++ sections/Idea.tsx
@@ -1,23 +1,24 @@
 import { MaskContainer } from '@/components/ui/mask-effect'
 import Link from 'next/link'
 import React from 'react'
+import { FiArrowUpRight } from 'react-icons/fi'
 
 const Idea = () => {
   return (
-    <section className="relative">
+    <section className="hidden md:block relative overflow-hidden lg:overflow-y-visible lg:overflow-x-clip">
       <video className="h-full scale-x-120 scale-y-111 w-full" src="/ropes.mp4" autoPlay loop muted playsInline />
       <div className="absolute z-20 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h4 className="text-4xl sm:text-5xl tracking-wide leading-wide mb-8 text-center font-light">Have <br /> an idea?</h4>
+        <h4 className="text-3xl lg:text-5xl tracking-wide leading-wide mb-4 lg:mb-8 text-center font-light">Have <br /> an idea?</h4>
         <div className="flex h-28 w-full items-center justify-center overflow-hidden">
           <MaskContainer
             revealText={
-              <Link href="/contact" className="rounded-full tracking-tight w-70 text-center text-5xl font-black text-white ">
+              <Link href="/contact" className="rounded-full tracking-tight w-70 text-center text-4xl lg:text-5xl font-black text-white ">
                 TELL US
               </Link>
             }
-            className="h-28 border-border font-black border rounded-full text-black "
+            className="h-24 lg:h-28 w-84 lg:w-94 border-border font-black border rounded-full text-black "
           >
-            <Link href="/contact" className="">TELL US</Link>
+            <Link href="/contact" className="flex items-center gap-2 uppercase">Let&apos;s build <FiArrowUpRight size={33} /> </Link>
           </MaskContainer>
         </div>
       </div>
