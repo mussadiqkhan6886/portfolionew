393eed30999859278a80a1777407db076488ae66
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-25 11:35:56 +0500

    starting skills

--- components/ui/Intro.tsx
+++ components/ui/Intro.tsx
@@ -51,8 +51,8 @@ export default function IntroLoader() {
             borderBottomRightRadius: "0%", }}
           exit={{
             y: "-100%",
-            borderBottomLeftRadius: "60%",
-            borderBottomRightRadius: "60%",
+            borderBottomLeftRadius: "50%",
+            borderBottomRightRadius: "50%",
             transition: {
               duration: 1.1,
               ease: [0.76, 0, 0.24, 1],
