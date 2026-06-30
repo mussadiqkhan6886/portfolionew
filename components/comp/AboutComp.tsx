96efdf51378cc2bc84d4fe282cb0cb15ff6510c8
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-30 13:11:53 +0500

    completed second aout section

--- components/comp/AboutComp.tsx
+++ components/comp/AboutComp.tsx
@@ -2,53 +2,45 @@
 
 import Image from 'next/image';
 import React, { useEffect, useRef } from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import FloatEffect from '../ui/FloatEffect';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const arrowRef = useRef<HTMLDivElement>(null);
   const imageBoxRef = useRef<HTMLDivElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
-    
+  let ctx = gsap.context(() => {
 
     gsap.fromTo(imageRef.current, 
-      { yPercent: -15 }, // Starts shifted up inside the overflow-hidden box
+      { yPercent: -15 },
       {
-        yPercent: 15,    // Ends shifted down
-        ease: "none",
+        yPercent: 15,
         scrollTrigger: {
           trigger: imageBoxRef.current,
-          start: "top bottom", // Starts when the top of the box enters the bottom of screen
-          end: "bottom top",   // Ends when the bottom of the box leaves the top of screen
-          scrub: true,        // Direct sync with scroll
+          scrub: true,
         }
       }
     );
 
-    return () => {
-      ScrollTrigger.getAll().forEach(t => t.kill());
-    };
-  }, []);
+  }, containerRef); 
+
+  return () => ctx.revert(); 
+}, []);
 
   return (
       <section ref={containerRef} className="flex flex-col md:flex-row gap-8 py-26 relative items-start">
         
-        <div ref={arrowRef} className="absolute -left-30 z-10">
-          <FiArrowRight size={32} />
-        </div>
-
-        <div className="w-full md:w-82 space-y-5 mr-5">
-          <p className=" text-black leading-tight">
+        <FloatEffect output={[-15, 50]}  className="w-full md:w-82 space-y-5 mr-5">
+          <p >
             I help companies from all over the world with tailor-made solutions. With each project, I push my work to new horizons, always putting quality first.
           </p>
           <p className="text-gray">Always Exploring...</p>
-        </div>
+        </FloatEffect>
 
         <div 
           ref={imageBoxRef} 
