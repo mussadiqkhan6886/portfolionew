4a6f1b5746df7d9d9df5de3739aed2c58c5467e9
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-07-09 12:49:31 +0500

    fixing some responsive issues

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import FloatEffect from '@/components/ui/FloatEffect'
+import MagnetText from '@/components/ui/MagnetEffect'
+import Link from 'next/link'
 import React from 'react'
 
 interface Params {
@@ -5,9 +8,30 @@ interface Params {
 }
 
 const SingleWork = async ({params}: Params) => {
+  const {slug} = await params
   return (
     <main>
-      
+      <section className="relative">
+        <h1>{slug}</h1>
+        <div>
+          <div>
+            <h2>Services</h2>
+          </div>
+          <div>
+            <h2>Location</h2>
+          </div>
+          <div>
+            <h2>STACK</h2>
+          </div>
+        </div>
+        <div className="absolute -bottom-15 right-10">
+          <FloatEffect output={[100,250]}>
+            <Link href={'/'}>
+              <MagnetText dot="no" text="Live Site" className="absolute w-38 h-38 items-center justify-center bg-ctr hover:bg-ctr-dark rounded-full text-white -bottom-5 right-8" />
+            </Link>
+          </FloatEffect>
+        </div>
+      </section>
     </main>
   )
 }
