74a175600c8af12475ca38eedec9ab0bc5460494
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-07-18 00:02:40 +0500

    starting seo

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <section style={{backgroundImage: `URL(${"/img3.webp"})`, backgroundPosition: "top", backgroundRepeat: "no-repeat"}} className="w-full h-dvh relative bg-gray/98 overflow-hidden bg-cover xl:bg-contain">
       <div className="absolute w-22 h-dvh left-0 bg-gray hidden lg:block" />
-      <div className="absolute w-screen h-screen inset-0 bg-black/10" />
+      <div className="absolute w-screen h-dvh inset-0 bg-black/10" />
       <FloatEffect output={[10, -150]} className="absolute flex justify-between w-full left-1/2 bottom-0 sm:top-[57%] -translate-x-1/2 sm:-translate-y-1/2 h-32 sm:h-37 items-center">
         <div className="bg-text text-white hidden sm:flex p-4 rounded-r-full items-center justify-between w-60">
           <div className="w-[50%] ml-7">
