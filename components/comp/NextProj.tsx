385a5b2a21bba2b02b134a78a187b64f8e3bc9eb
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-07-18 01:04:15 +0500

    fixed nextproj

--- components/comp/NextProj.tsx
+++ components/comp/NextProj.tsx
@@ -4,9 +4,8 @@ import React from 'react'
 import TransitionLink from './TransitionLink'
 import Image from 'next/image'
 import MagnetText from '../ui/MagnetEffect'
-import { useRef, useEffect, useState } from "react";
 import { AnimatePresence, easeInOut, motion } from "motion/react";
-import {gsap} from "gsap"
+import useCustomCursor from '@/lib/hooks/useCustomCursor';
 
 interface Props {
     name: string
@@ -16,33 +15,8 @@ interface Props {
 
 const NextProj = ({name, link, image}: Props) => {
 
-    const [isHovering, setIsHovering] = useState(false)
-    const containerRef = useRef<HTMLDivElement>(null);
-    const cursorRef = useRef<HTMLDivElement>(null);
-
-    const xTo = useRef<gsap.QuickToFunc | null>(null);
-    const yTo = useRef<gsap.QuickToFunc | null>(null);
-
-    useEffect(() => {
-        if (!cursorRef.current) return;
-        xTo.current = gsap.quickTo(cursorRef.current, "x", {
-        duration: 0.55,
-        ease: "power3.out",
-        });
-        yTo.current = gsap.quickTo(cursorRef.current, "y", {
-        duration: 0.55,
-        ease: "power3.out",
-        });
-    }, []);
-
-    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-        const rect = containerRef.current?.getBoundingClientRect();
-        if (!rect) return;
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        xTo.current?.(x);
-        yTo.current?.(y);
-    };
+    
+  const {containerRef, cursorRef, isHovering, setIsHovering, handleMouseMove} = useCustomCursor()
 
   return (
     <div className="bg-text pt-10 pb-3 md:pb-0 text-white">
