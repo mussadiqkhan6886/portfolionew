be70dc7959aff5330a930a2a2aa203f1e7ffaac1
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-07-20 19:39:15 +0500

    projecg loog

--- components/comp/ProjectThumb.tsx
+++ components/comp/ProjectThumb.tsx
@@ -18,7 +18,7 @@ const ProjectThumb = ({thumbnail, logo, title}: Props) => {
         <div className="relative w-full h-screen overflow-hidden" ref={imageBoxRef}>
             <Image ref={imageRef} src={thumbnail} priority={true} fetchPriority='high' alt={title + "thumbnail image"} width={1000} height={1000} className="absolute top-0 left-0 w-full h-[110%] md:h-[130%] object-cover object-center" />
         </div>
-        <Image src={logo as string} alt={title + "logo"} width={150} height={150} className="w-30 h-30 object-cover object-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+        <Image src={logo as string} alt={title + "logo"} width={150} height={150} className="w-30 h-30 object-contain object-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
     </div>
   )
 }
